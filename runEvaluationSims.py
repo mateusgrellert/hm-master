65a845fec54f8340b53d6f87a91061962b33f0cc
grellert <grellert@lab215-blacky.(none)>
2013-06-13 16:40:07 -0300

    added actuators to controller and started implementing auto_run for evaluations

--- runEvaluationSims.py
+++ runEvaluationSims.py
@@ -23,18 +23,24 @@ for sequence in sequence_list:
 		# start running each list here -- don't nest loops from this point on
 		for MaxPartitionDepth in MaxPartitionDepth_list:
 			os(execLine+' --MaxPartitionDepth='+MaxPartitionDepth)
+			wrapResults(sequence+"_"+qp+"_"+"maxDepth_"+MaxPartitionDepth)
 
 		for QuadtreeTUMaxDepthInter in QuadtreeTUMaxDepthInter_list:
 			os(execLine+' --QuadtreeTUMaxDepthInter='+QuadtreeTUMaxDepthInter)
+			wrapResults(sequence+"_"+qp+"_"+"TUDepth_"+QuadtreeTUMaxDepthInter)
 
 		for SearchRange in SearchRange_list:
 			os(execLine+' --SearchRange='+SearchRange)
+			wrapResults(sequence+"_"+qp+"_"+"SerchRange_"+SerchRange)
 
 		for HadamardME in HadamardME_list:
 			os(execLine+' --HadamardME='+HadamardME)
+			wrapResults(sequence+"_"+qp+"_"+"Had_"+HadamardME)
 
 		for AMP in AMP_list:
 			os(execLine+' --AMP='+AMP)
+			wrapResults(sequence+"_"+qp+"_"+"AMP_"+AMP)
 
 		for RDOQ in RDOQ_list:
 			os(execLine+' --RDOQ='+RDOQ)
+			wrapResults(sequence+"_"+qp+"_"+"RDOQ_"+RDOQ)
