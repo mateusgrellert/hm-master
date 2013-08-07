bbf6c9820141e45b553f4fce9e54437fd40d12db
grellert <grellert@lab215-blacky.(none)>
2013-08-07 16:53:38 -0300

    Cropped cfg files inserted

---
+++ cfg/per-sequence/cropCfgFiles.py
@@ -0,0 +1,37 @@
+from sys import argv
+from math import floor
+
+path = argv[1]
+print path
+
+if '10bit' in path:
+	print 'Skipping 10-bit cfgs'
+	exit()
+
+cfg = open(path,'r')
+new_cfg = open(path.split('.')[0]+'_cropped.cfg','w')
+
+lines = cfg.readlines()
+cfg.close()
+sw,sh = -1,-1
+
+for line in lines:
+	if 'InputFile' in line:
+		tokens = line.split('origCfP/')
+		inp_line = tokens[0] + 'origCfP/cropped/'
+		inp_yuv = tokens[1].split('_')[0]
+	elif 'SourceWidth' in line:
+		tokens = line.split(':')
+		sw = int(tokens[1].split()[0])
+		sw = int(floor(sw/64))*64
+		print >> new_cfg, tokens[0] + ' : ' + str(sw)
+	
+	elif 'SourceHeight' in line:
+		tokens = line.split(':')
+		sh = int(tokens[1].split()[0])
+		sh = int(floor(sh/64))*64
+		print >> new_cfg, tokens[0] + ' : ' + str(sh)
+	elif sw != -1 and sh != -1:
+		print >> new_cfg, inp_line+inp_yuv+'_'+str(sw)+'x'+str(sh)+'.yuv'
+	else:
+		print >> new_cfg, line
