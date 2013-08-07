7ee94e1e1e04985cf79ff74f17d3fd13ba97e4d4
grellert <grellert@lab215-blacky.(none)>
2013-08-07 12:58:23 -0300

    simulation script corrected

--- runEvaluationSims.py
+++ runEvaluationSims.py
@@ -2,7 +2,7 @@ from os import system
 
 def wrapResults(path):
 	system("mkdir results_"+path)
-	system("mv *csv ./results_"+path)
+	system("mv *csv *hsv ./results_"+path)
 
 
 sequence_list = ['PeopleOnStreet','Kimono','BasketballDrive','BlowingBubbles','ChinaSpeed']
@@ -19,10 +19,14 @@ RDOQ_list = ['0']
 
 execLine = './TAppEncoderStatic -c ../cfg/encoder_randomaccess_main.cfg'
 
-for sequence in sequence_list:
-	execLine += ' -c ../cfg/per-sequence/'+sequence+'.cfg'
+i = 0
 
-	for QP in QP_list:
+for sequence in sequence_list:
+    execLine += ' -c ../cfg/per-sequence/'+sequence+'.cfg'
+    execLine += ' --FramesToBeEncdoded=' + nFrames[i]
+    i += 1
+    
+    for QP in QP_list:
 		execLine += ' --QP='+QP
 
 		# start running each list here -- don't nest loops from this point on
